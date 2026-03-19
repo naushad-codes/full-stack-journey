85ad8df3d823b013386626955f0d7194f46f91dd
Naushad Ali <naushad.codes@gmail.com>
2026-03-19 23:25:45 +0530

    primitive and reference data type

--- Practice/WEEK_1/p1.js
+++ Practice/WEEK_1/p1.js
@@ -28,3 +28,29 @@ const b =15;
 // 1.Block scoped
 // 2.Cannot be reassigned
 
+const obj = {name:"naushad"};
+console.log(obj.name);
+
+obj.name="ali";
+
+//the const protects the reference not the data inside it hence the name inside obj can be changed.
+
+ /*
+ 🔹 Two types of memory:
+1. Stack (Primitive values)
+Fast
+Stores actual value 
+(number,bool,string,null,undefined,symbol,bigint)
+
+2. Heap (Reference values)
+Stores objects
+Variables store reference (address) 
+(arrays,object,functions)
+
+Shallow Copy:
+const user = { name: "Naushad" };
+const copy = { ...user };  spread operator
+copy.name = "Ali";
+console.log(user.name); 
+Naushad ✅
+  */

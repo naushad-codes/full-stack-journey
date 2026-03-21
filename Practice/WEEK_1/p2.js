97407719b81e571595c74bd91c6f9cf57bce505a
Naushad Ali <naushad.codes@gmail.com>
2026-03-21 23:58:30 +0530

    js day2

---
+++ Practice/WEEK_1/p2.js
@@ -0,0 +1,29 @@
+# Day 2 - Execution Context & Hoisting
+
+## Concepts Covered:
+- Execution Context
+- Memory Phase
+- Execution Phase
+- Hoisting
+- Temporal Dead Zone (TDZ)
+- Call Stack
+
+---
+
+## Q2 Explanation:
+Why let gives ReferenceError
+
+---
+
+## Q5 Explanation:
+Why output is undefined
+
+---
+
+## Q7 Explanation:
+Explain inner + outer behavior
+
+---
+
+## Q10 Explanation:
+Why error occurs (TDZ)

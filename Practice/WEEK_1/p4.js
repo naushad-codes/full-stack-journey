b236f8801b4ced1288b8ec9fe8909e231fcc28a0
Naushad Ali <naushad.codes@gmail.com>
2026-03-24 23:09:34 +0530

    p4 one

--- Practice/WEEK_1/p4.js
+++ Practice/WEEK_1/p4.js
@@ -1,3 +1,4 @@
 // i have studied the javascript variables. making using let , const and var 
 
 // i have also completed the javascript operators ,
+// also i have learned the javascript if conditions , loops , strings and numbers 

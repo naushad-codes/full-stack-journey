5f5f38460e4dc8e5f79524442eb5ae44b92dbbbc
Naushad Ali <naushad.codes@gmail.com>
2026-03-24 23:10:45 +0530

    p4 final

--- Practice/WEEK_1/p4.js
+++ Practice/WEEK_1/p4.js
@@ -2,3 +2,5 @@
 
 // i have also completed the javascript operators ,
 // also i have learned the javascript if conditions , loops , strings and numbers 
+
+// now i have completed this day and will continue from tomorrow
